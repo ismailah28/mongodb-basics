0168a153ae5bb62d4121d47d8e15d7e66298c23d
Ismaila Hassan <ismailah28@gmail.com>
2020-04-12 00:51:09 +0100

    create myMovies collection & insert movies into it

--- interns.js
+++ interns.js
@@ -1,11 +1,25 @@
 const { initDB } = require("./db");
 
-initDB(function(err, db) {
+const movies = [
+  { movie: "The Banker", year: "2020", rating: 8 },
+  { movie: "Bad Boys", year: "2020", rating: 7 },
+  { movie: "The Hunt", year: "2020", rating: 7 },
+  { movie: "Bloodshot", year: "2020", rating: 7.5 },
+  { movie: "First Cow", year: "2020", rating: 6.5 },
+];
+
+// initilize db and populate myMovies collection
+initDB(function (err, db) {
   if (err) {
     throw err;
   }
-  db.db("ismailah28").createCollection("interns", function(err, result) {
+  let dbo = db.db("ismailah28");
+
+  // insert movies into movies collection
+  dbo.collection("myMovies").insertMany(movies, function (err, res) {
     if (err) throw err;
-    console.log("interns collection created");
+    console.log(`${res.insertedCount} movies inserted.`);
+    console.log(res);
+    db.close();
   });
 });
